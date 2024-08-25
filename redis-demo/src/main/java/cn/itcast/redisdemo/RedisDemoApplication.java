81c64564c35d445942c82187218bfecfc1d25fa7
Huajieyu001 <331173500@qq.com>
2024-08-25 16:49:48 +0800

    哨兵模式

--- redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
+++ redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 
 @SpringBootApplication
 public class RedisDemoApplication {
@@ -13,4 +14,9 @@ public class RedisDemoApplication {
         SpringApplication.run(RedisDemoApplication.class, args);
     }
 
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer(){
+        return e -> e.readFrom(ReadFrom.REPLICA_PREFERRED);
+    }
 }
